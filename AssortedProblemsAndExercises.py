1d801276ad58159173f5a7dfb952fd5c07d4bcef
jocelynthiojaya <jocelynthiojaya@gmail.com>
2019-11-26 11:38:06 +0700

    AssortedProblems2

--- AssortedProblemsAndExercises.py
+++ AssortedProblemsAndExercises.py
@@ -5,3 +5,26 @@ Created on Tue Nov 26 11:34:45 2019
 @author: jocel
 """
 
+#1
+sampleData = "3, 5, 7, 23"
+sampleData = sampleData.replace(",","") 
+newList = sampleData.split()
+newTuple = tuple(newList)
+print(newList)
+print(newTuple)
+
+#2
+var_input = "this is fun"
+#print(var_input)
+vowel_list = ["a","i","u","e","o"," "]
+sentence = ""
+for vowel in var_input:
+   #print(vowel)
+    if vowel in vowel_list:
+        sentence = sentence + vowel
+        #print(vowel)
+    else:
+        sentence = sentence + vowel + 'o' + vowel
+
+
+print(sentence)
